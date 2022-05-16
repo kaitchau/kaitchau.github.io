6c87bdd666f9940873bbf2cb9015fda3c31fe4db
Kaitlyn Chau <kachau03@gmail.com>
2022-05-16 13:33:15 -0400

    added minor change to text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline">Work In Progress!</h1>
       </header>
     </div>
   );
