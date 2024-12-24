b3173d0d1c3e24ddf3c7eabe13d7d4b815fb19ca
Kaitlyn Chau <91744930+kaitchau@users.noreply.github.com>
2024-12-24 13:52:40 -0500

    set up sticky nav bar

---
+++ client/src/components/Home.js
@@ -0,0 +1,33 @@
+export default function Home() {
+    return(
+        <div>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            </div>
+    );
+}
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import '../stylesheets/NavBar.css';
+
+export default function NavBar() {
+    return(
+        <nav className='nav-bar'>
+            <a href='#'>Home</a>
+            <a href='#'>About</a>
+            <a href='#'>Contact</a>
+        </nav>
+    );
+}
