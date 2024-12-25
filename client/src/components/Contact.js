49ea1a8754e7bb80d6c44fda45196339ec6d4b62
Kaitlyn Chau <91744930+kaitchau@users.noreply.github.com>
2024-12-25 15:22:22 -0500

    updated contact

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -3,7 +3,12 @@ import '../stylesheets/Contact.css';
 export default function Contact() {
     return (
         <div id='contact' className='contact-container'>
-            <p>Hi from contact area</p>
+            <div className='contact-content'>
+                <h1>Contact</h1>
+                <span>Email: <a href='mailto:kaitlyn.m.chau@gmail.com'>kaitlyn.m.chau@gmail.com</a></span>
+                <span>LinkedIn: <a href='https://www.linkedin.com/in/kaitlyn-m-chau/'>https://www.linkedin.com/in/kaitlyn-m-chau/</a></span>
+                <span>GitHub: <a href='https://github.com/kaitchau'>https://github.com/kaitchau</a></span>
+            </div>
         </div>
     );
 }
