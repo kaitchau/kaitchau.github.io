a324639e2639057af17d9cf8104aa753d14bffd7
Kaitlyn Chau <91744930+kaitchau@users.noreply.github.com>
2024-12-24 21:11:53 -0500

    add .nojekyll to bypass jekyll

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,7 +5,7 @@ export default function About() {
         <div className="about-container">
             <div className='about'>
                 <h1>About Me</h1>
-                <p>More about me</p>
+                <p>Hello! I am a Senior pursuing a B.S. in Computer Science at Stony Brook University while working part-time as an Frontend Web Developer for SBU's Campus Residences IT. I aspire to gain professional experience within the software engineering, information technology, or cybersecurity field.</p>
             </div>
         </div>
     );
