2087334f9ee39969ea32af991c2f8c735d1f8c47
Kaitlyn Chau <91744930+kaitchau@users.noreply.github.com>
2024-12-27 19:41:45 -0500

    edits to project, edits to mobile view

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -5,13 +5,15 @@ export default function Contact() {
         <div id='contact' className='contact-container'>
             <div className='contact-content'>
                 <h1>Contact</h1>
-                <span>Email: <a href='mailto:kaitlyn.m.chau@gmail.com'>kaitlyn.m.chau@gmail.com</a> </span>
-                <span>LinkedIn: 
+                <span>Email:&nbsp;<a href='mailto:kaitlyn.m.chau@gmail.com'>kaitlyn.m.chau@gmail.com</a> </span>
+                <span>LinkedIn:&nbsp;
                     <img src={`${process.env.PUBLIC_URL}/linkedin-logo.png`} />
+                    &nbsp;
                     <a href='https://www.linkedin.com/in/kaitlyn-m-chau/'>https://www.linkedin.com/in/kaitlyn-m-chau/</a>
                 </span>
-                <span>GitHub: 
+                <span>GitHub:&nbsp;
                     <img src={`${process.env.PUBLIC_URL}/github-logo.png`} />
+                    &nbsp;
                     <a href='https://github.com/kaitchau'>https://github.com/kaitchau</a>
                 </span>
             </div>
--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -5,10 +5,13 @@ export default function Projects() {
         <div id='projects' className="projects-container">
             <div className='projects-content'>
                 <h1>Projects</h1>
-                <ul>
+                <ul className='projects-list'>
                     <li>
-                        <h2>Project 1</h2>
+                        <h2>Gerrymandering Analysis of U.S. Congressional District Plans</h2>
                         <p>Description of Project 1</p>
+                        <a href='https://votifier.net'>https://votifier.net</a>
+                        <br/>
+                        <a href='https://github.com/SBU-CSE-416/votifier'>https://github.com/SBU-CSE-416/votifier</a>
                     </li>
                     <li>
                         <h2>Project 2</h2>
